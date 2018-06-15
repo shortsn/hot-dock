a5e456e47191ba3e90824934d56a9ce81a919d33
shortsn <shortsn@users.noreply.github.com>
2018-06-15 20:50:12 +0200

    travis

--- src/ipc/redux/isAction.ts
+++ src/ipc/redux/isAction.ts
@@ -6,8 +6,8 @@ export interface IpcAction extends Action {
   meta?: any;
 }
 
+const isValidKey = (key) => ['type', 'payload', 'error', 'meta'].indexOf(key) > -1;
+
 export default function isAction(arg: any): arg is IpcAction {
   return typeof (arg) === 'object' && typeof (arg.type) === 'string' && Object.keys(arg).every(isValidKey);
 }
-
-const isValidKey = (key) => ['type', 'payload', 'error', 'meta'].indexOf(key) > -1;
