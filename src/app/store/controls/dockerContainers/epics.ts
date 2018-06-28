1716a8230dcf250cc81d99f2da132d6bc43cad7f
shortsn <shortsn@users.noreply.github.com>
2018-06-28 22:32:30 +0200

    epic

--- src/app/store/controls/dockerContainers/epics.ts
+++ src/app/store/controls/dockerContainers/epics.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActionsObservable } from 'redux-observable';
 
-import { timer } from 'rxjs/observable/timer';
-import { filter, startWith, map, flatMap, scan, switchMap, tap, throttleTime } from 'rxjs/operators';
-import { isUpdateLocationAction, RouterUpdateLocationAction } from '../../location/model';
-import { empty } from 'rxjs/observable/empty';
+import { filter } from 'rxjs/operators';
 import { DockerActions } from '../../data/docker/actions';
-import { merge } from 'rxjs/observable/merge';
 
 @Injectable()
 export class DockerContainersEpics {
@@ -14,24 +10,6 @@ export class DockerContainersEpics {
   loadContainers = (action$: ActionsObservable<any>) =>
     action$
       .pipe(
-        filter(action => isUpdateLocationAction(action)),
-        switchMap<RouterUpdateLocationAction, any>(action => action.payload.startsWith('/info/containers')
-          ? merge(
-              timer(0, 60000).pipe(map(_ => DockerActions.DOCKER_FETCH_CONTAINERS({}))),
-            action$
-              .pipe(
-                filter(a =>
-                  DockerActions.is.DOCKER_START_CONTAINER_SUCCESS(a) ||
-                  DockerActions.is.DOCKER_STOP_CONTAINER_SUCCESS(a) ||
-                  DockerActions.is.DOCKER_PAUSE_CONTAINER_SUCCESS(a) ||
-                  DockerActions.is.DOCKER_UNPAUSE_CONTAINER_SUCCESS(a) ||
-                  DockerActions.is.DOCKER_KILL_CONTAINER_SUCCESS(a)
-                ),
-                map(_ => DockerActions.DOCKER_FETCH_CONTAINERS({}))
-              ),
-            )
-          : empty()
-        )
+        filter(DockerActions.is.DOCKER_EVENT),
+        filter(_ => false)
       )
-
-}
